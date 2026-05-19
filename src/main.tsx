8fcd235b0470ec1ddf5ac286c9b669e6d831c93e
Shottopoth Recode <recode@shottopoth.app>
2026-05-19 21:41:58 +0800

    Add @vercel/analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 import App from "./App.tsx";
 import { AppWrapper } from "./components/common/PageMeta.tsx";
@@ -10,6 +11,7 @@ createRoot(document.getElementById("root")!).render(
     <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
       <AppWrapper>
         <App />
+        <Analytics />
       </AppWrapper>
     </ThemeProvider>
   </StrictMode>
