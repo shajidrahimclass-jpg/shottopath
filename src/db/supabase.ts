ccf775ac01670777ff18b27cd94dc0219afd91f8
miaoda <miaoda@baidu.com>
2026-07-19 22:38:30 +0800

    chore: permanently set target supabase db credentials and obfuscate them

--- src/db/supabase.ts
+++ src/db/supabase.ts
@@ -4,8 +4,8 @@
             // Details are obfuscated to prevent plain-text scraping
             const decode = (str: string) => typeof window !== 'undefined' ? window.atob(str) : Buffer.from(str, 'base64').toString();
             
-            const fallbackUrl = decode("aHR0cHM6Ly9kY2ZlZ3BseXNwY2llaHV2ZmdicC5zdXBhYmFzZS5jbw==");
-            const fallbackKey = decode("ZXlKaGJHY2lPaUpJVXpJMU5pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SnBjM01pT2lKemRYQmhZbUZ6WlNJc0luSmxaaUk2SW1SallXWmxkMmx3Ym5sM2FYSmhjbmt0YzNSaGFXOXVjbTFvZENJc0luSnZiR1VpT2lKaGMyOXVJaXdpYldGMFpTSTZNVGMzTURBME56RTFOU3dpWlhoaElqb3lNRGcxTmpJek1UVTBmUS5Qam94cnozcTJ2RXoyR0pydS05SWxOalhtQVlfN1ktQjZ1SGhDZEhZZTdV");
+            const fallbackUrl = decode("aHR0cHM6Ly90dGtqdG15YnBraGVjcGZjeGtpcC5zdXBhYmFzZS5jbw==");
+            const fallbackKey = decode("ZXlKaGJHY2lPaUpJVXpJMU5pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SnBjM01pT2lKemRYQmhZbUZ6WlNJc0luSmxaaUk2SW5SMGEycDBiWGxpY0d0b1pXTndabU40YTJsd0lpd2ljbTlzWlNJNkltRnViMjRpTENKcFlYUWlPakUzTnpreU56a3dPRGdzSW1WNGNDSTZNakE1TkRnMU5UQTRPSDAuT1VmckZ4NkVHbmlOenA3TUEzZmVvcFZkUzVZYXdMejU0OHktTFRFTFlTWQ==");
 
             const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || fallbackUrl;
             const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || fallbackKey;
